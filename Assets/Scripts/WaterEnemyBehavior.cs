9f5d4964794c94f459ab587e532d0a629569c742
Sam Tregillus <samtregillus@gmail.com>
2018-11-17 11:53:25 -0600

    Water enemy working

--- Assets/Scripts/WaterEnemyBehavior.cs
+++ Assets/Scripts/WaterEnemyBehavior.cs
@@ -9,9 +9,18 @@ public enum EnemyState {
 
 public class WaterEnemyBehavior : MonoBehaviour {
 
-	public EnemyState State;
+	public EnemyState state;
 
-	public Transform TargetTransform;
+	public Transform targetTransform;
+
+	public float strokeCooldown;
+	public Animator animator;
+	public Rigidbody2D rb;
+
+	public float StrokeImpulse = 10;
+
+	private bool _pointAtPlayer = false;
+	private float _strokeCooldownTimer = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -20,7 +29,7 @@ public class WaterEnemyBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		switch (State) {
+		switch (state) {
 			case EnemyState.Idle:
 				IdleState();
 				break;
@@ -35,26 +44,54 @@ public class WaterEnemyBehavior : MonoBehaviour {
 	}
 
 	void ChaseState() {
-		PointAtPlayer();
+		
+		if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) {
+			_pointAtPlayer = true;
+		}
+
+		if (_pointAtPlayer) {
+			PointAtPlayer();
+		}
+
 		MoveTowardsPlayer();
 	}
 
 	void PointAtPlayer() {
-		Vector2 toTarget = TargetTransform.position - transform.position;
-		transform.right = toTarget.normalized;
+		Vector2 toTarget = targetTransform.position - transform.position;
+		float angle = Mathf.Atan2(toTarget.y, toTarget.x) * Mathf.Rad2Deg;
+		Quaternion targetRot = Quaternion.AngleAxis(angle, Vector3.forward);
+
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, .3f);
+
+		//transform.right = toTarget.normalized;
 	}
 
 	void MoveTowardsPlayer() {
+		_strokeCooldownTimer -= Time.deltaTime;
+		if(_strokeCooldownTimer <= 0) {
+			animator.SetTrigger("SwimStroke");
+			_strokeCooldownTimer = strokeCooldown;
+			
+		}
+	}
 
+	//for potential pathfinding in the future
+	Vector2 DetermineDirection() {
+		return (targetTransform.position - transform.position).normalized;
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision) {
 		if (collision.tag == "Player") {
-			State = EnemyState.Chasing;
+			state = EnemyState.Chasing;
 		}
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision) {
 		print("ahhhh");
+	}
+
+	void SwimImpulse() {
+		_pointAtPlayer = false;
+		rb.AddForce(transform.right * StrokeImpulse, ForceMode2D.Impulse);
 	}
 }
